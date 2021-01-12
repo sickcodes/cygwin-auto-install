ea4a6468a20d37eea69721fa6e9dbd60c14232c0
sickcodes <65906298+sickcodes@users.noreply.github.com>
2021-01-12 18:38:40 +0000

    Add openssh,rsync,fail2ban
    Oceania Mirrors
    
    Missing space
    
    Package list change
    
    Python
    
    Replace svn with curl
    
    Use cygwin commands
    
    wget
    
    Tidy
    
    No brackets
    
    Authorized IPs

--- cygwin-install.bat
+++ cygwin-install.bat
@@ -1,7 +1,26 @@
 @ECHO OFF
 REM -- Automates cygwin installation
 REM -- Source: https://github.com/rtwolf/cygwin-auto-install
+REM -- Contrib: https://github.com/sickcodes/cygwin-auto-install
 REM -- Based on: https://gist.github.com/wjrogers/1016065
+rem cygwin-auto-install
+rem ===================
+
+rem Automated cygwin install. Just download the project as a zip file, extract and run cygwin-install.bat to install cygwin + apt-cyg + packages required for apt-cyg + optional packages.
+
+rem You can edit the batch file to specify which optional packages you'd like installed.
+
+rem Created by wjrogers: https://gist.github.com/wjrogers/1016065.
+rem Updated by sickcodes: https://twitter.com/sickcodes/.
+
+rem Suggest this workflow for this project: http://scottchacon.com/2011/08/31/github-flow.html
+
+rem Source URL:
+rem https://github.com/rtwolf/cygwin-auto-install/
+
+rem If you've found this project helpful, please support me by buying me a coffee: http://www.mind-manual.com/blog/buy-me-a-coffee/
+
+rem As you may know, "A programmer is just a tool which converts caffeine into code". Thanks in advance!
  
 SETLOCAL
  
@@ -17,23 +36,29 @@ IF NOT EXIST cygwin-setup.exe (
 )
  
 REM -- Configure our paths
-SET SITE=http://cygwin.mirrors.pair.com/
+SET MIRROR=http://mirror.internode.on.net/pub/cygwin/
+SET BACKUP_MIRROR=http://mirror.internode.on.net/pub/cygwin/
 SET LOCALDIR=%CD%
 SET ROOTDIR=C:/cygwin
+SET RUN_SSHD=true
+SET SSH_PORT=22
+rem SET AUTHORIZED_IPS=10.15.97.0/24,10.17.0.0/16
+SET AUTHORIZED_IPS=10.0.0.0/16
  
-REM -- These are the packages we will install (in addition to the default packages)
-SET PACKAGES=mintty,wget,ctags,diffutils,git,git-completion,git-svn,stgit,mercurial
+REM -- These are the packages we will install in addition to the default packages
+REM -- Package list here: http://cygwin.com/packages/package_list.html
+SET PACKAGES=base-cygwin,base-files,mintty,cron,wget,curl,vim,diffutils,git,bash,bash-completion,openssh,sshpass,zstd,python3,python3-pip,rsync,zip
 REM -- These are necessary for apt-cyg install, do not change. Any duplicates will be ignored.
 SET PACKAGES=%PACKAGES%,wget,tar,gawk,bzip2,subversion
  
 REM -- More info on command line options at: https://cygwin.com/faq/faq.html#faq.setup.cli
 REM -- Do it!
 ECHO *** INSTALLING DEFAULT PACKAGES
-cygwin-setup --quiet-mode --no-desktop --download --local-install --no-verify -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%"
+cygwin-setup --quiet-mode --no-desktop --download --local-install --no-verify -s %MIRROR% -l "%LOCALDIR%" -R "%ROOTDIR%" || cygwin-setup --quiet-mode --no-desktop --download --local-install --no-verify -s %BACKUP_MIRROR% -l "%LOCALDIR%" -R "%ROOTDIR%"
 ECHO.
 ECHO.
 ECHO *** INSTALLING CUSTOM PACKAGES
-cygwin-setup -q -d -D -L -X -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P %PACKAGES%
+cygwin-setup -q -d -D -L -X -s %MIRROR% -l "%LOCALDIR%" -R "%ROOTDIR%" -P %PACKAGES%
  
 REM -- Show what we did
 ECHO.
@@ -43,11 +68,14 @@ ECHO  - %PACKAGES%
 ECHO.
 
 ECHO apt-cyg installing.
-set PATH=%ROOTDIR%/bin;%PATH%
-%ROOTDIR%/bin/bash.exe -c 'svn --force export http://apt-cyg.googlecode.com/svn/trunk/ /bin/'
-%ROOTDIR%/bin/bash.exe -c 'chmod +x /bin/apt-cyg'
-ECHO apt-cyg installed if it says somin like "A    /bin" and "A   /bin/apt-cyg" and "Exported revision 18" or some other number.
-
+set PATH=%ROOTDIR%/bin;%CD%;%PATH%
+%ROOTDIR%/bin/bash.exe -c 'wget https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg -O /bin/apt-cyg'
+%ROOTDIR%/bin/bash.exe -c 'chmod a+x /bin/apt-cyg'
+%ROOTDIR%/bin/bash.exe -c '/bin/apt-cyg --version'
+%ROOTDIR%/bin/bash.exe -c 'mkpasswd'
+%ROOTDIR%/bin/bash.exe -c 'ssh-host-config --port %SSH_PORT% --yes'
+%ROOTDIR%/bin/bash.exe -c '[ "${RUN_SSHD}" ] && net start sshd'
+netsh advfirewall firewall add rule name="Open Port %SSH_PORT%" dir=in action=allow protocol=TCP localport=%SSH_PORT% remoteip=%AUTHORIZED_IPS%
 ENDLOCAL
  
 PAUSE
